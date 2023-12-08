44829182a35782cd8b67ce7ea1d05524ea83b1ca
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-12-08 18:59:45 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PureCareHub_HospitalCare.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
