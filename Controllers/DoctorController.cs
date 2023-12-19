80beb374c542dcefe3df28bdc423509bbfa84fda
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-12-20 01:13:04 +0300

    made a beautifull design and edit and adding doctors

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using PureCareHub_HospitalCare.Models;
 using PureCareHub_HospitalCare.Data;
 using PureCareHub_HospitalCare.ViewModels;
 using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+using PureCareHub_HospitalCare.Models.Service;
 
 namespace PureCareHub_HospitalCare.Controllers
 {
@@ -16,7 +17,7 @@ namespace PureCareHub_HospitalCare.Controllers
         //private readonly 
         [Obsolete]
         public DoctorController(ApplicationDBContext dbContext,
-                                IWebHostEnvironment webHostEnvironment) 
+                                IWebHostEnvironment webHostEnvironment)
         { 
             _dbContext = dbContext;
             _webHostEnvironment = webHostEnvironment;
@@ -50,14 +51,77 @@ namespace PureCareHub_HospitalCare.Controllers
                 _dbContext.Add(doctor);
                 _dbContext.SaveChanges();
                 TempData["Success"] = "Successfully created a Doctor";
-                return RedirectToAction("Index");
+                return RedirectToAction("List");
             }
             return View();
         }
-        [HttpGet]
-        public ViewResult Detail(DoctorRegistrationViewModel model)
+
+        public IActionResult Edit(int? docid)
         {
+            if (docid == null || docid == 0)
+            {
+                return NotFound();
+            }
+
+            Doctor? doctordb = _dbContext.doctors.Find(docid);
+
+            //category? categoryfromdb1 = _dbcontext.categories.firstordefault(u=>u.categoryid == id);
+            ////category? categoryfromdb2 = _dbcontext.categories.where(u=>u.categoryid==id).firstordefault();
+
+            if (doctordb == null)
+            {
+                return NotFound();
+            }
+
+            return View(doctordb);
+        }
+        [HttpPost]
+
+        public IActionResult Edit(Doctor doctor, DoctorEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFilename = "";
+
+                if (model.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    uniqueFilename = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFilename);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+
+                    doctor.PhotoPath = uniqueFilename;
+                }
+                _dbContext.Add(doctor);
+                _dbContext.SaveChanges();
+                TempData["Success"] = "Successfully created a Doctor";
+                return RedirectToAction("List");
+            }
             return View();
         }
+
+        //public IActionResult Edit(Doctor obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //Update the new objects
+        //        _dbContext.Update(obj);
+        //        _dbContext.SaveChanges();
+        //        TempData["Success"] = "Successfully updated a Category";
+
+        //        return RedirectToAction("List");
+        //    }
+        //    else
+        //    {
+        //        return View(obj);
+        //    }
+        //}
+        public IActionResult List()
+        {
+            var model = _dbContext.doctors.ToList();
+            return View(model);
+        }
+
+       
     }
 }
---
+++ ViewModels/DoctorEditViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PureCareHub_HospitalCare.ViewModels
+{
+    public class DoctorEditViewModel :DoctorRegistrationViewModel
+    {
+        public int Id { get; set; } 
+        public string ExistingPhotoPath { get; set; } = string.Empty;
+    }
+}
